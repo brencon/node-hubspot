5a52090c383900f234e43cb31925a3282adaa2ff
Brendan Conrad <BrConrad@jhacorp.com>
2019-02-11 18:49:03 -0800

    completed default config json

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+'use strict';
+const _ = require('lodash');
+const config = require('config');
+const rp = require('request-promise');
 
+console.log('src/index.js');
+
+/*
+module.exports.searchByHashtag = function(hashtag) {
+  const apiURL = `${config.instagram.baseURI}/${config.instagram.apiVersion}`;
+  const options = {
+    method: 'GET',
+    uri: `${apiURL}/users/self/media/recent?access_token=${config.instagram.accessToken}`,
+    rejectUnauthorized: config.rejectUnauthorized
+  };
+  return rp(options).then(function(res) {
+    const matchedMedia = [];
+    const resJSON = JSON.parse(res);
+    _.forEach(resJSON.data, function(d) {
+      if (d.tags.indexOf(hashtag) > -1)
+        matchedMedia.push(d);
+
+    });
+    return { data: matchedMedia };
+  });
+};
+*/
