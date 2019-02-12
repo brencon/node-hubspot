45355a4aea3a23e548d9608c7716b631620ae4a8
Brendan Conrad <BrConrad@jhacorp.com>
2019-02-11 19:50:51 -0800

    completed hubspot api dev with working test

--- src/index.js
+++ src/index.js
@@ -10,8 +10,27 @@ look up the contact by vid
 get the contact name and email
  */
 
-module.exports.getAllTickets = function() {
-  const apiURL = `${config.hubspot.baseURI}crm-objects/v1/objects/tickets/paged?hapikey=${config.hubspot.apikey}&properties=subject&properties=content&properties=created_by`;
+let _apiKey = '';
+
+function getContactByID(ticketObj) {
+  const apiURL = `${config.hubspot.baseURI}contacts/v1/contact/vid/${ticketObj.contactId}/profile?hapikey=${_apiKey}`;
+  const options = {
+    method: 'GET',
+    uri: `${apiURL}`,
+    rejectUnauthorized: config.rejectUnauthorized
+  };
+  return rp(options).then(function(res) {
+    const resJSON = JSON.parse(res);
+    ticketObj.firstName = resJSON.properties.firstname.value;
+    ticketObj.lastName = resJSON.properties.lastname.value;
+    ticketObj.email = resJSON.properties.email.value;
+    return ticketObj;
+  });
+}
+
+module.exports.getAllTickets = function(apiKey) {
+  _apiKey = apiKey;
+  const apiURL = `${config.hubspot.baseURI}crm-objects/v1/objects/tickets/paged?hapikey=${apiKey}&properties=subject&properties=content&properties=created_by`;
   const options = {
     method: 'GET',
     uri: `${apiURL}`,
@@ -22,10 +41,21 @@ module.exports.getAllTickets = function() {
     const resJSON = JSON.parse(res);
     _.forEach(resJSON.objects, function(obj) {
       let hsTicket = {};
-
+      hsTicket.subject = obj.properties.subject.value;
+      hsTicket.contactId = obj.properties.created_by.value;
+      hsTicket.body = obj.properties.content.value;
       hsTickets.push(hsTicket);
     });
-    return hsTickets;
+
+    const actions = hsTickets.map(getContactByID);
+
+    const results = Promise.all(actions);
+
+    return results.then(function(hsTicketsWithContact) {
+      console.log(hsTicketsWithContact);
+      return hsTicketsWithContact;
+    });
+
   });
 };
 
--- test/src/index.spec.js
+++ test/src/index.spec.js
@@ -5,7 +5,7 @@ var nodeHubSpot = require('../../index');
 
 describe('node-hubspot', function() {
   it('works', function() {
-    nodeHubSpot.getAllTickets().then(function(res) {
+    nodeHubSpot.getAllTickets('yourkeygoesherelol').then(function(res) {
       const hsData = res;
       expect(hsData).to.be.an('array').that.is.not.empty;
     });
